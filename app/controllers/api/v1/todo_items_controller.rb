8b14890c0b657563378d4979d37442325e25937f
Bruno Montebello <brunormontebello@hotmail.com>
2020-11-17 00:39:52 +0000

    Create authorized endpoints

--- app/controllers/api/v1/todo_items_controller.rb
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::TodoItemsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -18,7 +19,21 @@ class Api::V1::TodoItemsController < ApplicationController
   end
 
   private
+
     def set_todo_item
       @todo_item = TodoItem.find(params[:id])
     end
+
+    def authorized?
+      @todo_item.user == current_user
+    end
+
+    def handle_unauthorizes
+      unless authorized?
+        respond_to do |format|
+          format.json { render :unauthorized, status: 401 }
+        end
+      end
+    end
+
 end
